9e43c88809f15d3132b566f20e594c35f1f38a08
rshell-handler <dr.leha20121987@gmail.com>
2026-08-30 11:34:03 +0500

    Update patches.sh

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -391,3 +391,5 @@ if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
 		*) die "unknown patch step '$1'" ;;
 	esac
 fi
+sed -i '/qrb5165-iot-rb5.dtb/d' $WORKSPACE/android-kernel/arch/arm64/boot/dts/vendor/qcom/Makefile
+
